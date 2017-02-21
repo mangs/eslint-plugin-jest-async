1adbccfa1b6f1d5df588bb41ef49e864403bad6d
Eric L. Goldstein <eric@call-em-all.com>
2017-02-21 16:03:55 -0600

    Fixed improper flagging of dispatch chains with more than one '.then()'

--- tests/lib/rules/expect-return.js
+++ tests/lib/rules/expect-return.js
@@ -1,7 +1,7 @@
 /**
  * @fileoverview Tests to enforce returning store.dispatch() when asynchronously making a test
  *               assertion
- * @author Eric L. Goldstein
+ * @author Eric L. Goldstein <elg2001@columbia.edu>
  */
 'use strict';
 
@@ -26,12 +26,41 @@ RuleTester.setDefaultConfig({
 const ruleTester = new RuleTester();
 ruleTester.run('expect-return', rule, {
   valid: [
-    'function test() { return store.dispatch(action()).then(() => expect(store.getActions()).toEqual(expectedActions)); }'
+    `
+    function test() {
+      return store.dispatch(action())
+        .then(() => expect(store.getActions()).toEqual(expectedActions)); }
+    `,
+    `
+    function test() {
+      return store.dispatch(action())
+        .then(() => store.dispatch(action2()))
+        .then(() => expect(store.getActions()).toEqual(expectedActions));
+    }
+    `
   ],
 
   invalid: [
     {
-      code: 'function test() { store.dispatch(action()).then(() => expect(store.getActions()).toEqual(expectedActions)); }',
+      code: `
+        function test() {
+          store.dispatch(action())
+            .then(() => expect(store.getActions()).toEqual(expectedActions));
+        }
+      `,
+      errors: [{
+        message: 'Return async dispatch expressions',
+        type: 'CallExpression',
+      }],
+    },
+    {
+      code: `
+        function test() {
+          store.dispatch(action())
+            .then(() => store.dispatch(action2()))
+            .then(() => expect(store.getActions()).toEqual(expectedActions));
+        }
+      `,
       errors: [{
         message: 'Return async dispatch expressions',
         type: 'CallExpression',
